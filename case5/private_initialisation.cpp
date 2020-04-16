e1f672d6e75726227355e06176dd9f2f6a4364fe
lellakeerthi <63228412+lellakeerthi@users.noreply.github.com>
2020-04-16 12:17:48 +0530

    Add files via upload

---
+++ case5/private_initialisation.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+class Rectangle
+{
+	//Private member variables
+	int ilength,ibreadth,ilen,ibre;
+	public:
+		Rectangle()
+		{
+		ilen = 20 ;
+		ibre = 10 ;
+		}
+		
+		Rectangle(int il, int ib)
+		{
+			ilength=il ;
+			ibreadth=ib ;
+			cout<<"ilength: "<<ilength<<endl;
+			cout<<"ibreadth:"<<ibreadth<<endl;
+			cout<<"Area of rectangle :"<<il*ib<<endl;
+		}
+	int Var()
+	{
+		cout << "length:" << ilen << endl ;
+		cout << "breadth:" << ibre << endl ;
+	}		
+};
+
+int main(int argc,char * argv[])
+{
+	
+	if((argc==2) && (strcmp(argv[1],"-h")==0))	 				
+	{
+    	cout<<" usage of"<< argv[0] << endl;
+        cout<< " initilization of private variables" << endl ;
+	}
+    
+    else
+    {
+    	//Creating object to class A
+    	Rectangle ob;
+    	ob.Var() ;
+		Rectangle ob1(4, 5);
+		
+	}
+}
+
---
+++ case5/private_scope.cpp
@@ -0,0 +1,57 @@
+/*PROGRAM:Initialisation of private variable using scope resolution 
+filename:private_scope.cpp
+date:15-04-2020
+Author:l.keerthi
+*/
+#include<iostream>
+#include<string.h>
+using namespace std;
+class Rectangle
+{ 
+    int iLength;
+	int ibreadth;
+	static int iLen;
+	static int iBre;
+public:
+	/*function name:calculatearea
+	return type:void
+	input parameter:int ,int
+	*/
+    void calculatearea( int iLength, int ibreadth )
+    {
+                  
+    	int Area;
+		Area = ( iLength * ibreadth );
+        cout<< "Area of rectangle:" << Area << endl;
+    }
+	/*function name:calculateperimeter
+	return type:void
+	input parameter:int ,int
+	*/
+	void calculateperimeter()
+	{
+		int iPeri;
+		iPeri = 2*( iLen + iBre );
+		cout<< "perimeter of rectangle:" << iPeri << endl;
+	}
+};
+//private data member initialization using scope resolution .
+     int Rectangle::iLen = 90;
+     int Rectangle::iBre = 80;
+     
+int main( int argc, char *argv[] )
+{
+	
+    if((argc==2) && (strcmp(argv[1],"-h")==0))
+	{
+		cout << " usage of " << argv[0] << endl;
+		cout<< " private member initilization using scope resolution" << endl ;
+	}
+	else
+	{
+    	Rectangle rect;
+		rect.calculatearea(8,9);
+		rect.calculateperimeter();
+        return 0;
+	}
+}
