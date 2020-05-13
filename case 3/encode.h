e7f462a4810a74ce8de8ea2f45bf097bb2497891
lellakeerthi <63228412+lellakeerthi@users.noreply.github.com>
2020-05-13 15:21:15 +0530

    Update encode.h

--- case 3/encode.h
+++ case 3/encode.h
@@ -9,10 +9,15 @@ using namespace std ;
 //using namespace std ;
 class code
 {
-		public:
-			void encrypt(char* filename, char* key) ;
-			void decrypt(char* filename) ;
-			void Secret_key(char* filename, char* key) ;
+	//private members
+	private:
+		char alpha ;
+	//public members
+	public:
+		code() ;	
+		void encrypt(char* filename, char* key) ;
+		void decrypt(char* filename) ;
+		void Secret_key(char* filename, char* key) ;
 };
 #endif 
 
