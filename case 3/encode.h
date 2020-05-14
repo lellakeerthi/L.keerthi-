c20c03c90218bcc02e47f5eab2db34b90b963e43
lellakeerthi <63228412+lellakeerthi@users.noreply.github.com>
2020-05-14 20:31:43 +0530

    Update encode.h

--- case 3/encode.h
+++ case 3/encode.h
@@ -1,23 +1,22 @@
 #ifndef CODE_H
 #define CODE_H
 
-#include<iostream>
-#include<fstream>
-#include<string.h>
-using namespace std ; 
-
-//using namespace std ;
 class code
 {
 	//private members
 	private:
 		char alpha ;
+		char word ;
+		char str[100] ;
+		int count ;
+		
+
 	//public members
 	public:
+	
 		code() ;	
 		void encrypt(char* filename, char* key) ;
 		void decrypt(char* filename, char* key) ;
-
+		
 };
 #endif 
-
