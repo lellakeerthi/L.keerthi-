f5fb0b8317fff0d4a78477a28762b76dda89578e
lellakeerthi <63228412+lellakeerthi@users.noreply.github.com>
2020-05-29 17:30:28 +0530

    Update main.cpp

--- case 3/main.cpp
+++ case 3/main.cpp
@@ -11,75 +11,56 @@ using namespace std ;
 
 int main(int argc,char *argv[])  
 {
-	int var ;
-	encode obj ;
+	int find ;
+	encode encodeObj ;
 	// checking arguments to display -h
-    	if((argc==2) && (strcmp(argv[1],"-h")==0))
+    if((argc==2) && (strcmp(argv[1],"-h")==0))
 	{
 		cout << " usage of"<< argv[0] <<endl;           
-		cout << "-f filename -e encryption / -d decryption -k key " << endl ; 		
+		cout << "-f filename -e encryption / -d decryption -k key " << endl ;	
 	}
 		
-	else if(argc==6)
+	else if(argc>2) 
 	{
+		char* filename; 
+		char* key;
 		
-			// conditions for encryption
-			if (strcmp(argv[1],"-e")==0) 
+		if((strcmp(argv[1],"-e")==0) || (strcmp(argv[1],"-d")==0))
+		{
+		
+			//for finding -f & -k
+			for (int indx = 0; indx < argc; indx++)
 			{
-				if ((strcmp(argv[2],"-f")==0 ) )
-				{	
-					var=obj.encrypt(argv[3], argv[5]) ;
-					if(var==1)
-					
-						cout<<"encrypt function executed" ;
-					
-					else
-					
-						cout<<"encryption failed" ;
-				}
+				if (strcmp(argv[indx], "-f") == 0)
+					filename = argv[indx + 1];
+				else  if (strcmp(argv[indx], "-k") == 0)
+					key = argv[indx + 1];
 			}
-				else if((strcmp(argv[2], "-k")==0))
-				{
-				
-					var=obj.encrypt(argv[5], argv[3]) ;		
-					if(var==1)
-					
-						cout<<"encrypt function executed" ;
-					
-					else
-					
-						cout<<"encryption failed" ;
-				}
-		
-			// conditions for decryption		
-			else if (strcmp(argv[1],"-d")==0)
+			//encryption
+			if (strcmp(argv[1], "-e") == 0)
 			{
-				//if ()
-				if ((strcmp(argv[2],"-f")==0 ) )
-				{	
-					var=obj.decrypt(argv[3], argv[5]) ;
-					if(var==1)
-					
-						cout<<"encrypt function executed" ;
-					
-					else
-					
-						cout<<"encryption failed" ;
-				}
-				else if((strcmp(argv[2], "-k")==0))
-				{
-					var=obj.decrypt(argv[5], argv[3]) ;		
-	
-					if(var==1)
-					
-						cout<<"encrypt function executed" ;
-					
-					else
-					
-						cout<<"encryption failed" ;
+				find = encodeObj.encrypt(filename, key);
+				if (find == 1)
+					cout << "encryption executed" << endl;
+				else
+					cout << "encryption not executed" << endl;
+			}
+			//decryption
+			else if (strcmp(argv[1], "-d") == 0)
+			{
+				find = encodeObj.decrypt(filename, key);
+				if (find == 1)
+					cout << "decryption executed" << endl;
+				else
+					cout << "decryption not executed" << endl;
+			}
+		}
+		else
+		{
+			cout<<"invalid arguments" ;
+		}
 
-				}
-			}
+			
 	}
 	else
 	{
